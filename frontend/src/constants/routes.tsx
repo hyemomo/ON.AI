36a23e0f8cc4a2f9bf281ab8a48eb61ab4c209aa
hyemin <hyemin6647@naver.com>
2026-05-16 14:34:34 +0900

    ✨ feature: 커뮤니티와 게시물 상세 라우트 추가

--- frontend/src/constants/routes.tsx
+++ frontend/src/constants/routes.tsx
@@ -1,9 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import ChatPage from "../pages/ChatPage";
+import CommunityPage from "@/pages/CommunityPage";
+import PostDetailPage from "@/pages/PostDetailPage";
 
 export const router = createBrowserRouter([
   {
     path: "/chat",
     element: <ChatPage />,
   },
+  { path: "/community", element: <CommunityPage /> },
+  { path: "/community/1", element: <PostDetailPage /> },
 ]);
