535a4ae003040c36471edbc932922e21a663a582
hyemin <hyemin6647@naver.com>
2026-05-21 15:42:25 +0900

    ✨ feature: 커뮤니티 게시물 응답 type과 카테고리 추가

--- frontend/src/features/community/post-detail/types/types.ts
+++ frontend/src/features/community/post-detail/types/types.ts
@@ -1,20 +1,26 @@
 export interface Post {
-  id: number;
-  author: string;
-  authorLevel?: string;
-  childAge: string;
-  timeAgo: string;
-  category: string;
-  categoryColor: string;
-  title: string;
-  body: string;
-  emoji: string;
-  images?: string[];
-  likes: number;
-  comments: number;
-  views: number;
-  isHot?: boolean;
-  liked?: boolean;
+  postnum: number;
+  p_user: number;
+
+  p_title: string;
+  p_content: string;
+
+  p_region_tag: string;
+  p_category_tag: string;
+
+  nickname: string;
+
+  p_created_at: string;
+
+  image_urls: string[];
+
+  like_count: number;
+  comment_count: number;
+  is_liked: boolean;
+}
+
+export interface CommunityPostsResponse {
+  posts: Post[];
 }
 
 export interface Comment {
@@ -29,4 +35,3 @@ export interface Comment {
   liked?: boolean;
   isAuthor?: boolean;
 }
-
